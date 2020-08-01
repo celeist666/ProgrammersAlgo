8703a45f690297ff1cf1242b1dec45777bf00127
celeist666 <athebate@gmail.com>
2020-08-02 01:22:19 +0900

    Update 가장 큰 수.py

--- 가장 큰 수.py
+++ 가장 큰 수.py
@@ -1,13 +1,7 @@
-def solution1(prices):
-    answer = [0] * len(prices)
-    stack = []
-    for i in range(len(prices)):
-        while stack and prices[i] < prices[stack[len(stack) -1]]:
-            temp = stack.pop()
-            answer[temp] = i - temp
-        stack.append(i)
-    while len(stack):
-        temp = stack.pop()
-        answer[temp] = len(prices) - temp - 1
+# 1000이하니까 3자리라고 치고 스트링을 3배로 늘린 뒤 이를 sorting하면 사전순으로 정렬되기에
+# 원하는 순서대로 정렬이 된다. 
 
-    return answer
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
