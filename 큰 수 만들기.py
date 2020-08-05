95b75097455cda1525ac8964f834955a5e7f48ad
celeist666 <athebate@gmail.com>
2020-08-05 21:07:01 +0900

    Update 큰 수 만들기.py

--- 큰 수 만들기.py
+++ 큰 수 만들기.py
@@ -1,6 +1,6 @@
 # 앞에서부터 비교해서 다음 수가 더 크면 해당 자리의 수를 지운다.
 # 다음 숫자가 더 크면 스택에서 빼주는 식
-def solution(number, k):
+def solution(number, k): 
     stack = [number[0]]
     for num in number[1:]:
         while stack and stack[-1] < num and k > 0:
