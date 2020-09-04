7a95ce5a7f84ef977f72d90cf1163811e043b73f
celeist666 <athebate@gmail.com>
2020-09-04 20:14:31 +0900

    Update N개의 최소공배수.py

--- N개의 최소공배수.py
+++ N개의 최소공배수.py
@@ -1,8 +1,8 @@
 from fractions import gcd
 
 def nlcm(num):
-    answer = num[0]
-    for n in num: 
+    answer = num[0] 
+    for n in num:
         answer = n * answer / gcd(n, answer)
 
     return answer
